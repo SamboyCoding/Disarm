9f2061e964875002deab31259b8c892ddf1f25ce
Sam Byass <sam@byass.me>
2026-08-07 23:57:17 +0100

    Implement remaining hints

--- Disarm.Tests/HintTests.cs
+++ Disarm.Tests/HintTests.cs
@@ -37,6 +37,48 @@ public class HintTests : BaseDisarmTest
         => DisassembleAndCheckMnemonic(0xD50320DF, Arm64Mnemonic.DGH);
     
     [Fact]
-    public void TestXpaclri() 
+    public void TestXpaclri()
         => DisassembleAndCheckMnemonic(0xD50320FF, Arm64Mnemonic.XPACLRI);
+
+    [Fact]
+    public void TestPacAndAutHints()
+    {
+        DisassembleAndCheckMnemonic(0xD503211F, Arm64Mnemonic.PACIA1716);
+        DisassembleAndCheckMnemonic(0xD503215F, Arm64Mnemonic.PACIB1716);
+        DisassembleAndCheckMnemonic(0xD503219F, Arm64Mnemonic.AUTIA1716);
+        DisassembleAndCheckMnemonic(0xD50321DF, Arm64Mnemonic.AUTIB1716);
+        DisassembleAndCheckMnemonic(0xD503231F, Arm64Mnemonic.PACIAZ);
+        DisassembleAndCheckMnemonic(0xD503233F, Arm64Mnemonic.PACIASP);
+        DisassembleAndCheckMnemonic(0xD503235F, Arm64Mnemonic.PACIBZ);
+        DisassembleAndCheckMnemonic(0xD503237F, Arm64Mnemonic.PACIBSP);
+        DisassembleAndCheckMnemonic(0xD503239F, Arm64Mnemonic.AUTIAZ);
+        DisassembleAndCheckMnemonic(0xD50323BF, Arm64Mnemonic.AUTIASP);
+        DisassembleAndCheckMnemonic(0xD50323DF, Arm64Mnemonic.AUTIBZ);
+        DisassembleAndCheckMnemonic(0xD50323FF, Arm64Mnemonic.AUTIBSP);
+    }
+
+    [Fact]
+    public void TestBarrierAndConsistencyHints()
+    {
+        DisassembleAndCheckMnemonic(0xD503221F, Arm64Mnemonic.ESB);
+        DisassembleAndCheckMnemonic(0xD503223F, Arm64Mnemonic.PSB_CSYNC);
+        DisassembleAndCheckMnemonic(0xD503225F, Arm64Mnemonic.TSB_CSYNC);
+        DisassembleAndCheckMnemonic(0xD503229F, Arm64Mnemonic.CSDB);
+    }
+
+    [Fact]
+    public void TestBti()
+    {
+        DisassembleAndCheckMnemonic(0xD503241F, Arm64Mnemonic.BTI);
+        DisassembleAndCheckMnemonic(0xD503245F, Arm64Mnemonic.BTI_C);
+        DisassembleAndCheckMnemonic(0xD503249F, Arm64Mnemonic.BTI_J);
+        DisassembleAndCheckMnemonic(0xD50324DF, Arm64Mnemonic.BTI_JC);
+    }
+
+    [Fact]
+    public void TestUnallocatedHint()
+    {
+        var insn = DisassembleAndCheckMnemonic(0xD50325BF, Arm64Mnemonic.HINT);
+        Assert.Equal("0x00000000 HINT 0x2D", insn.ToString());
+    }
 }
--- Disarm/InternalDisassembly/Arm64Hints.cs
+++ Disarm/InternalDisassembly/Arm64Hints.cs
@@ -1,4 +1,4 @@
-﻿namespace Disarm.InternalDisassembly;
+namespace Disarm.InternalDisassembly;
 
 internal static class Arm64Hints
 {
@@ -6,57 +6,54 @@ internal static class Arm64Hints
     {
         var crm = (instruction >> 8) & 0b1111; // Bits 8-11
         var op2 = (instruction >> 5) & 0b111; // Bits 5-7
+        var hintNum = crm << 3 | op2;
 
-        return crm switch
+        var mnemonic = hintNum switch
         {
-            0b0000 when op2 == 0b000 => new()
+            0b0000_000 => Arm64Mnemonic.NOP,
+            0b0000_001 => Arm64Mnemonic.YIELD,
+            0b0000_010 => Arm64Mnemonic.WFE,
+            0b0000_011 => Arm64Mnemonic.WFI,
+            0b0000_100 => Arm64Mnemonic.SEV,
+            0b0000_101 => Arm64Mnemonic.SEVL,
+            0b0000_110 => Arm64Mnemonic.DGH,
+            0b0000_111 => Arm64Mnemonic.XPACLRI, //FEAT_PAUTH
+            0b0001_000 => Arm64Mnemonic.PACIA1716,
+            0b0001_010 => Arm64Mnemonic.PACIB1716,
+            0b0001_100 => Arm64Mnemonic.AUTIA1716,
+            0b0001_110 => Arm64Mnemonic.AUTIB1716,
+            0b0010_000 => Arm64Mnemonic.ESB,
+            0b0010_001 => Arm64Mnemonic.PSB_CSYNC,
+            0b0010_010 => Arm64Mnemonic.TSB_CSYNC,
+            0b0010_100 => Arm64Mnemonic.CSDB,
+            0b0011_000 => Arm64Mnemonic.PACIAZ,
+            0b0011_001 => Arm64Mnemonic.PACIASP,
+            0b0011_010 => Arm64Mnemonic.PACIBZ,
+            0b0011_011 => Arm64Mnemonic.PACIBSP,
+            0b0011_100 => Arm64Mnemonic.AUTIAZ,
+            0b0011_101 => Arm64Mnemonic.AUTIASP,
+            0b0011_110 => Arm64Mnemonic.AUTIBZ,
+            0b0011_111 => Arm64Mnemonic.AUTIBSP,
+            0b0100_000 => Arm64Mnemonic.BTI,
+            0b0100_010 => Arm64Mnemonic.BTI_C,
+            0b0100_100 => Arm64Mnemonic.BTI_J,
+            0b0100_110 => Arm64Mnemonic.BTI_JC,
+            _ => Arm64Mnemonic.HINT, //unallocated hints execute as nops, so just show the number
+        };
+
+        if (mnemonic == Arm64Mnemonic.HINT)
+            return new()
             {
-                Mnemonic = Arm64Mnemonic.NOP,  
+                Mnemonic = Arm64Mnemonic.HINT,
+                Op0Kind = Arm64OperandKind.Immediate,
+                Op0Imm = hintNum,
                 MnemonicCategory = Arm64MnemonicCategory.Hint,
-            },
-            0b0000 when op2 == 0b001 => new()
-            {
-                Mnemonic = Arm64Mnemonic.YIELD,  
-                MnemonicCategory = Arm64MnemonicCategory.Hint,
-            },
-            0b0000 when op2 == 0b010 => new()
-            {
-                Mnemonic = Arm64Mnemonic.WFE,  
-                MnemonicCategory = Arm64MnemonicCategory.Hint,
-            },
-            0b0000 when op2 == 0b011 => new()
-            {
-                Mnemonic = Arm64Mnemonic.WFI,  
-                MnemonicCategory = Arm64MnemonicCategory.Hint,
-            },
-            0b0000 when op2 == 0b100 => new()
-            {
-                Mnemonic = Arm64Mnemonic.SEV,  
-                MnemonicCategory = Arm64MnemonicCategory.Hint,
-            },
-            0b0000 when op2 == 0b101 => new()
-            {
-                Mnemonic = Arm64Mnemonic.SEVL,  
-                MnemonicCategory = Arm64MnemonicCategory.Hint,
-            },
-            0b0000 when op2 == 0b110 => new()
-            {
-                Mnemonic = Arm64Mnemonic.DGH,  
-                MnemonicCategory = Arm64MnemonicCategory.Hint,
-            },
-            //FEAT_PAUTH
-            0b0000 when op2 == 0b111 => new()
-            {
-                Mnemonic = Arm64Mnemonic.XPACLRI,  
-                MnemonicCategory = Arm64MnemonicCategory.Hint,
-            },
-            
-            //TODO Remaining hints (crm != 0)
-            _ => new()
-            {
-                Mnemonic = Arm64Mnemonic.UNIMPLEMENTED,  
-                MnemonicCategory = Arm64MnemonicCategory.Hint,
-            },
+            };
+
+        return new()
+        {
+            Mnemonic = mnemonic,
+            MnemonicCategory = Arm64MnemonicCategory.Hint,
         };
     }
 }
